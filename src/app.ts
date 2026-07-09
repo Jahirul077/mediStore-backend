91f07f66743aedb28e575d39d82584219f3125ee
Johirul <jahirulislamrayhan07@gmail.com>
2026-07-09 11:56:33 +0600

    Category api done

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { authRouter } from "./modules/auth/auth.router";
 import { sellerRouter } from "./modules/seller/seller.router";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 import { medicinesRouter } from "./modules/Medicines/medicines.router";
+import { categoriesRouter } from "./modules/categories/categories.router";
 
 const app: Application = express();
 
@@ -15,6 +16,8 @@ app.use("/api/auth", authRouter);
 app.use("/api/seller", sellerRouter);
 
 app.use("/api/medicines", medicinesRouter);
+
+app.use("/api/categories", categoriesRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
